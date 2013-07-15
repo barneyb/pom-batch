e1ce2e5b5bd6f175b9eaf36346ead4f8c38bed69
barneyb <bboisvert@gmail.com>
2013-07-15 08:52:21 -0700

    mirror script to rip the blackmilkclothing.com site

---
+++ mirror.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# I mirror blackmilkclothing.com
+
+COOKIES=./cookies.txt
+LOG=./log/mirror.`date "+%F_%T"`.log
+USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36"
+
+cd `dirname $0`
+
+touch $LOG
+
+wget \
+	--append-output=$LOG \
+	--background \
+	--load-cookies $COOKIES \
+	--save-cookies $COOKIES \
+	--mirror \
+	--exclude-domains=blog.blackmilkclothing.com \
+	--wait=0.25s \
+	--random-wait \
+	--reject .js,.css \
+	"http://blackmilkclothing.com/collections?theme=main"
+
+#	--user-agent="$USER_AGENT" \
+
+tail -f $LOG
