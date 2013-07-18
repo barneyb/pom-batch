0c95d09ec048bbd660145c2663a6330add244b7c
barneyb <bboisvert@gmail.com>
2013-07-18 08:47:35 -0700

    * added category for the brand  * deduced existence of a 'limited' category from the brand and title  * canonicalized categories  * fixed indentation

--- process.js
+++ process.js
@@ -45,72 +45,95 @@ function work(f, jsf) {
                 docBody += s;
 				});
 			res.on('end', function() {
-					var prior = '',
-                        doc;
-					if (exists) {
-						prior = docBody;
-						doc = JSON.parse(docBody);
-//						if (doc.thumbprint == thumbprint) {
-//							log(f, 'no local changes...');
-//							return; // no local change
-//						}
-						doc.brand = json.brand;
-					} else {
-						doc = json;
-					}
-                    var cats = categories.filter(function(it) {
-                            return it.indexOf(f + ":") == 0;
-                        }).map(function(it) {
-                            return it.split(":")[1];
-                        });
-                    cats.push(doc.brand.indexOf("Black Milk ") == 0 ? doc.brand.substr(11) : doc.brand);
-                    if (! doc.categories) {
-                        doc.categories = [];
-                    }
-                    cats.forEach(function(cat) {
-                        if (doc.categories.indexOf(cat) < 0) {
-                            doc.categories.push(cat);
-                        }
+                var prior = '',
+                    doc;
+                if (exists) {
+                    prior = docBody;
+                    doc = JSON.parse(docBody);
+//                    if (doc.thumbprint == thumbprint) {
+//                        log(f, 'no local changes...');
+//                        return; // no local change
+//                    }
+                    doc.brand = json.brand;
+                } else {
+                    doc = json;
+                }
+
+                // cats from the list
+                var cats = categories.filter(function(it) {
+                        return it.indexOf(f + ":") == 0;
+                    }).map(function(it) {
+                        return it.split(":")[1];
                     });
-					doc.thumbprint = thumbprint;
-					var $ = cheerio.load(fs.readFileSync(dir + '/' + f));
-					doc.hashtag = $('span.tag').first().text();
-					if (json.thumbnail_url) {
-						var parts = json.thumbnail_url.split('?');
-						parts = parts[0].split('.');
-						doc.image = {
-							root: parts.slice(0, parts.length - 1).join('.'),
-							ext: parts[parts.length - 1]
-						};
-					}
-					delete doc.thumbnail_url;
-					delete doc.product_id;
-					doc = JSON.stringify(doc);
-					if (doc == prior) {
-						log(f, 'document is unchanged');
-						return; // no local change
-					}
-					http.request({
-							hostname: HOST,
-							port: PORT,
-							auth: AUTH,
-							method: 'PUT',
-							path: DB + f
-						},
-						function(res) {
-							res.on('data',
-								   function(s) {
-									   log(f, 'save: ' + s);
-								   });
-							res.on('end',
-								   function() {}
-								   );
-						})
-						.on('error',
-							function() {
-								log(f, 'error: ' + JSON.stringify(arguments));
-							})
-						.end(doc);
+
+                // cat from the brand
+                var bc = doc.brand;
+                if (bc.indexOf("Black Milk ") == 0) {
+                    bc = bc.substr(11);
+                }
+                if (bc.indexOf("Limited ") == 0) {
+                    cats.push("Limited");
+                    bc = bc.substr(8);
+                }
+                cats.push(bc);
+
+                // cats from the title
+                if (doc.title.indexOf("LIMITED") >= 0) {
+                    cats.push("limited");
+                }
+
+                if (! doc.categories) {
+                    doc.categories = [];
+                }
+                cats.map(function(cat) {
+                    return cat.toLowerCase()
+                        .replace(/([0-9])-([0-9])/g, "$1_$2")
+                        .replace(/-/g, " ")
+                        .replace(/_/g, "-");
+                }).forEach(function(cat) {
+                    if (cat != "black milk" && doc.categories.indexOf(cat) < 0) {
+                        doc.categories.push(cat);
+                    }
+                });
+                doc.thumbprint = thumbprint;
+                var $ = cheerio.load(fs.readFileSync(dir + '/' + f));
+                doc.hashtag = $('span.tag').first().text();
+                if (json.thumbnail_url) {
+                    var parts = json.thumbnail_url.split('?');
+                    parts = parts[0].split('.');
+                    doc.image = {
+                        root: parts.slice(0, parts.length - 1).join('.'),
+                        ext: parts[parts.length - 1]
+                    };
+                }
+                delete doc.thumbnail_url;
+                delete doc.product_id;
+                doc = JSON.stringify(doc);
+                if (doc == prior) {
+                    log(f, 'document is unchanged');
+                    return; // no local change
+                }
+                http.request({
+                        hostname: HOST,
+                        port: PORT,
+                        auth: AUTH,
+                        method: 'PUT',
+                        path: DB + f
+                    },
+                    function(res) {
+                        res.on('data',
+                               function(s) {
+                                   log(f, 'save: ' + s);
+                               });
+                        res.on('end',
+                               function() {}
+                               );
+                    })
+                    .on('error',
+                        function() {
+                            log(f, 'error: ' + JSON.stringify(arguments));
+                        })
+                    .end(doc);
 				});
 		});
 	req.on('error', function(){
