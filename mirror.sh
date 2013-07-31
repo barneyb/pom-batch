e9b895789f328bc7c6d62751a07742a09b60ac8b
barneyb <bboisvert@gmail.com>
2013-07-30 20:08:22 -0700

    removed the pre-fetch for the theme cookie in favor of just setting the desired cookie header manually.  also tried to exclude the mobile links, but not sure if it'll work.

--- mirror.sh
+++ mirror.sh
@@ -2,7 +2,6 @@
 #
 # I mirror blackmilkclothing.com
 
-COOKIES=./cookies.txt
 LOG=./log/mirror.`date "+%F_%T"`.log
 USER_AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36"
 
@@ -13,24 +12,17 @@ cd `dirname $0`
 # record the script that was used to trigger
 cat $0 > $LOG
 
-# make one request to ensure it's the right theme
-wget \
-	--load-cookies $COOKIES \
-	--save-cookies $COOKIES \
-	"http://blackmilkclothing.com/collections?theme=main"
-
-rm collections # this was just fetched, but we don't care
-
 # start it (in the background)
 wget \
 	--append-output=$LOG \
 	--background \
-	--load-cookies $COOKIES \
+	--no-cookies \
+	--header "Cookie: theme=main" \
 	--mirror \
 	--exclude-domains=blog.blackmilkclothing.com \
 	--wait=1s \
 	--random-wait \
-	--reject .js,.css \
+	--reject ".js,.css,\?theme=mobile" \
 	"http://blackmilkclothing.com/collections" \
 	"http://blackmilkclothing.com/collections/all"
 
